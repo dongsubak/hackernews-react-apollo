e8fe92fee9282f5f47f4d2034b8f81e9ad1744a4
unknown <blackxub@naver.com>
2018-10-06 21:31:40 +0900

    Routing fin

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,24 @@ import logo from '../logo.svg';
 import '../styles/App.css';
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="center w85">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <CreateLink />
-        <LinkList />
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+          </Switch>
+        </div>
       </div>
     );
   }
