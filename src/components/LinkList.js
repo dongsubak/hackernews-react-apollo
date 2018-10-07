0ca6f9af46266c90cd9abd878c4a8c0198d7b3f6
dongsubak <likeap@gmail.com>
2018-10-07 21:38:33 +0900

    Mutation Updating the Store fin

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,7 +3,7 @@ import Link from './Link'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
@@ -11,38 +11,40 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id 
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
 `
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
   render() {
-    /*
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API 😎 😎',
-        url: 'https://www.prismagraphql.com',
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ]
-    */
     return (
       <Query query={FEED_QUERY}>
         {({loading, error, data}) => {
-          console.log(error)
           if (loading) return <div>Fetching</div>
           if (error) return <div>Error</div>
           
           const linksToRender = data.feed.links
           return (
             <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+              {linksToRender.map((link, index) => (<Link key={link.id} link={link} index={index} updateCacheAfterVote={this._updateCacheAfterVote} />))}
             </div> 
           )
           }
